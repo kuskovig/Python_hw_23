111ebda120fe88d35b522b1142c35096323bd246
IlyaKuskov <ilya.kuskov@rubius.com>
2021-10-15 13:58:55 +0700

    small fixes, txt output

--- parser.py
+++ parser.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 import subprocess
+import time
 
-result = subprocess.run(["ps",
+output = subprocess.run(["ps",
                          "axo",
                          "user:20,pcpu,rss,comm:20",
                          "--sort", "-%cpu",
-                         "--no-heading"], capture_output=True)
-output = result.stdout.decode("utf-8")
-list_of_prc = [i.split() for i in output.split("\n")]
-list_of_prc.pop()  #remove empty item at the end
+                         "--no-heading"], capture_output=True). \
+    stdout.decode("utf-8")
+list_of_processes = [i.split() for i in output.split("\n")]
+list_of_processes.pop()  # remove empty item at the end
 
 '''
-join process names that contain of more than one word
+Parses list of entries, where every entry is list of columns data
+
+Join process names that contain of more than one word
 since we set output to 4 columns only, 4+ columns are parts of name of process
-'''
-for i in list_of_prc:
-    while len(i) > 4:
-        i[len(i)-2] += f" {i.pop()}"
 
-'''
-gathers info about users, total cpu and mem usage
+Counts: processes by username (0 item), total cpu (1st item) and mem usage(2nd item)
+
 '''
 prc_users = {}
 total_cpu = 0
 total_mem = 0
-for i in list_of_prc:
+
+for i in list_of_processes:
+    while len(i) > 4:
+        i[len(i) - 2] += f" {i.pop()}"
     if i[0] in prc_users.keys():
         prc_users[i[0]] += 1
     else:
@@ -37,19 +39,23 @@ Creates a string with listed user and amount of his processes. Each user on new 
 '''
 processes_per_user = "\r\n".join([f"{i[0]}: {i[1]}" for i in sorted(prc_users.items(),
                                                                     key=lambda x: x[1], reverse=True)])
-total_processes = len(list_of_prc)
-total_mem_mb = total_mem/1000
-'''
-Creates a string of all users with processes separated by comma
-'''
-system_users = ", ".join([i for i in prc_users.keys()])
+total_processes = len(list_of_processes)  # Total amount of processes is len of list
+total_mem_mb = total_mem / 1000  # Kb to MB conversion
+system_users = ", ".join([i for i in prc_users.keys()])  # Combine info about users and processes into string
 
-print(f"~~ System Users: {system_users} ~~")
-print(f"~~ Total running processes: {total_processes} ~~")
-print(f"~~ Processes per user:\r\n{processes_per_user }")
-print(f"~~ Total Memory in use = {'%.2f'%total_mem_mb}Mb ~~")
-print(f"~~ Total CPU in use = {'%.2f'%total_cpu}% ~~")
-print(f"~~ Most CPU is used by: {list_of_prc[0][3]}")
-print(f"~~ Most MEM is used by: {sorted(list_of_prc, key=lambda x: x[2], reverse=True)[0][3]}")
+output_dict = {
+    "System Users": f"{system_users}\r\n",
+    "\r\nTotal running processes": f"{total_processes}\r\n",
+    "\r\nProcesses per user": f"\r\n{processes_per_user}\r\n",
+    "\r\nTotal Memory in use": f"{'%.2f' % total_mem_mb}Mb\r\n",
+    "\r\nTotal CPU in use": f"{'%.2f' % total_cpu}%\r\n",
+    "\r\nMost CPU is used by": f"{list_of_processes[0][3]}\r\n",
+    "\r\nMost MEM is used by": f"{sorted(list_of_processes, key=lambda x: x[2], reverse=True)[0][3]}"
+}
 
+for i, j in output_dict.items():
+    print(f"{i}: {j}", end='')
 
+with open(f"{time.strftime('%d-%m-%y-%H:%M', time.localtime())}-stats.txt", "w") as out:
+    for i, j in output_dict.items():
+        out.write(f"{i}: {j}")
